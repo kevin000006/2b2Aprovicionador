5a820f9ea62813d079500b90599324048b7a4421
Omar Loarte <olarte.ex@bcloud.com.pe>
2020-04-21 14:14:01 -0500

    cambio de ruta para el login

--- src/app/main/pages/authentication/login-2/login-2.service.ts
+++ src/app/main/pages/authentication/login-2/login-2.service.ts
@@ -7,8 +7,8 @@ import { UsuarioModel } from '../../../../model/Usuario';
     providedIn: 'root'
 })
 export class LoginService {
-    private readonly urlBase: string = "http://localhost:8080/api";
-    //private readonly urlBase: string = "https://cors-anywhere.herokuapp.com/https://webapp-b2b.azurewebsites.net/api";
+    //private readonly urlBase: string = "http://localhost:8080/api";
+    private readonly urlBase: string = "https://cors-anywhere.herokuapp.com/https://webapp-b2b.azurewebsites.net/api";
 
     constructor(private http: HttpClient) { }
 
