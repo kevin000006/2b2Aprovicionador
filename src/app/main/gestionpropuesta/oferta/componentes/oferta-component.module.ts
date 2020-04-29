71bed29d0295998bf21b5601144d2e803e1f6bd3
Mike <ccarhuas.mike@gmail.com>
2020-04-29 15:24:29 -0500

    nuevo cambios

--- src/app/main/gestionpropuesta/oferta/componentes/oferta-component.module.ts
+++ src/app/main/gestionpropuesta/oferta/componentes/oferta-component.module.ts
@@ -1,33 +1,34 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
-import {MatMenuModule} from '@angular/material/menu';
-import {OfertaCabeceraModule} from './oferta-cabecera/oferta-cabecera.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { OfertaCabeceraModule } from './oferta-cabecera/oferta-cabecera.module';
 import { OfertaEquipamientoModule } from './oferta-equipamiento/oferta-equipamiento.module';
 import { OfertaGastosModule } from './oferta-gastos/oferta-gastos.module';
-
-
+import { OfertaServicioModule } from './oferta-servicio/oferta-servicio..module';
 
 @NgModule({
 
     declarations: [
-        ],
-    imports     : [
+    ],
+    imports: [
         OfertaCabeceraModule,
         OfertaEquipamientoModule,
         OfertaGastosModule,
+        OfertaServicioModule,
         MatInputModule,
         MatIconModule,
         MatTableModule,
         MatMenuModule
     ],
-    exports:[
+    exports: [
         OfertaCabeceraModule,
         OfertaEquipamientoModule,
-        OfertaGastosModule
+        OfertaGastosModule,
+        OfertaServicioModule
     ]
 })
-export class OfertaComponentModule{
+export class OfertaComponentModule {
 
 }
