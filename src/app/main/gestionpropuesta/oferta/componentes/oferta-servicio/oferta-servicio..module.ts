00d7d8b753939ca34d50f424aa7d2094a1ac8da8
Mike <ccarhuas.mike@gmail.com>
2020-05-03 18:25:44 -0500

    cambios en la bandeja de servicios

--- src/app/main/gestionpropuesta/oferta/componentes/oferta-servicio/oferta-servicio..module.ts
+++ src/app/main/gestionpropuesta/oferta/componentes/oferta-servicio/oferta-servicio..module.ts
@@ -1,21 +1,32 @@
+
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { OfertaServicioComponent } from './oferta-servicio.component'
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [
         OfertaServicioComponent
     ],
     imports: [
-        MatIconModule,
+        FormsModule,
+        CommonModule,
+        MatIconModule,        
         MatTableModule,
         MatMenuModule,
         MatPaginatorModule,
-        MatSortModule
+        MatSortModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule        
     ],
     exports: [
         OfertaServicioComponent
