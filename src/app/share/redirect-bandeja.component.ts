90f818988c215fc78dd78e5c553b848ba1cfe40d
Mike <ccarhuas.mike@gmail.com>
2020-05-06 18:38:03 -0500

    se quito los debugger;

--- src/app/share/redirect-bandeja.component.ts
+++ src/app/share/redirect-bandeja.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
     selector: 'app-redirect-bandeja',
     templateUrl: './shared.component.html',
     styleUrls: ['./shared.component.scss']
-  })
+})
 
 export class RedirectBandejaComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute,
-        private _router: Router) {}
+    constructor(
+        private route: ActivatedRoute,
+        private _router: Router) { }
+    ngOnInit(): void {
+        let _base = JSON.parse(localStorage.getItem('bandeja')) || [];
+        this.route.paramMap.subscribe(params => {
+            let _params = params['params'];
+            let data = _base.find(x => x.codigo == _params['id']);
 
-
-        ngOnInit(): void {
-            debugger;
-            let _base = JSON.parse(localStorage.getItem('bandeja')) || [];
-            this.route.paramMap.subscribe(params => {
-                let _params= params['params'];
-                let data = _base.find(x => x.codigo == _params['id']);
-
-                   this._router.navigate([data.url], { state: data.model });     
-             });
-
-        }
+            this._router.navigate([data.url], { state: data.model });
+        });
+    }
 
 }
