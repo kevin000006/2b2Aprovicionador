bf8121eada55238274d1e1c63bd539fc23d91582
omar5919 <250186se>
2020-05-09 20:39:29 -0500

    descarga de adjuntos

--- src/app/main/gestionpropuesta/oferta/componentes/fileinput/fileinput.service.ts
+++ src/app/main/gestionpropuesta/oferta/componentes/fileinput/fileinput.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams  } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,32 +7,32 @@ import { Observable } from 'rxjs';
 })
 export class FileInputService {
 
-  constructor(private http: HttpClient) {     
-  }  
-  deleteFileContainers(id: string): Observable<any> {        
-    return this.http.post<FormData>('/Containers/deleteFileContainers/'+id +'', {});    
+  constructor(private http: HttpClient) {
   }
-  downLoadFileContainers(data: any): Observable<any> {    
-    return this.http.post<FormData>('/Containers/downLoadFileContainers', data,{  });    
+  deleteFileContainers(id: string): Observable<any> {
+    return this.http.post<FormData>('/Containers/deleteFileContainers/' + id + '', {});
   }
-  listFilesContainers(): Observable<any> {    
-    return this.http.post<any>('/Containers/listFilesContainers',{});    
-  }  
-  uploadToContainers(data: FormData): Observable<any> {    
-    return this.http.post<FormData>('/Containers/uploadToContainers', data,{  
-      reportProgress: true,  
-      observe: 'events'  
-    });    
-  }   
+  downLoadFileContainers(data: any): Observable<any> {
+    return this.http.get<FormData>('/Containers/downLoadFileContainers/' + data);
+  }
+  listFilesContainers(): Observable<any> {
+    return this.http.post<any>('/Containers/listFilesContainers', {});
+  }
+  uploadToContainers(data: FormData): Observable<any> {
+    return this.http.post<FormData>('/Containers/uploadToContainers', data, {
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
 }
 
 
 /*
 public upload(formData) {
 
-	return this.httpClient.post<any>(this.SERVER_URL, formData, {  
-      reportProgress: true,  
-      observe: 'events'  
-    });  
+	return this.httpClient.post<any>(this.SERVER_URL, formData, {
+      reportProgress: true,
+      observe: 'events'
+    });
 }
 */
