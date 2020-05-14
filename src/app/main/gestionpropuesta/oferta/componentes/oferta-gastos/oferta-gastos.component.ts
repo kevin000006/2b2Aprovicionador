5e9935d7db4c8a14aa6ee49e5a339dc0a07f60e2
Mike <ccarhuas.mike@gmail.com>
2020-05-13 19:39:01 -0500

    cambios en el modulo de gastos

--- src/app/main/gestionpropuesta/oferta/componentes/oferta-gastos/oferta-gastos.component.ts
+++ src/app/main/gestionpropuesta/oferta/componentes/oferta-gastos/oferta-gastos.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-//import { CurrencyPipe } from '@angular/common';
-
+import { Component, OnInit, Input } from '@angular/core';
 import { AlertConfirmComponent } from '../alertConfirm/alertConfirm.component';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { OfertaGastosService } from './oferta-gastos.service';
-
-
+import { CommonService } from 'app/common.service';
+import { OfertaOpex } from '../../../../../model/Common';
 @Component({
   selector: 'oferta-gastos',
   templateUrl: './oferta-gastos.component.html',
@@ -16,49 +14,38 @@ export class OfertaGastosComponent implements OnInit {
   tipoCambio: number = 3.5;
   isPrepositionChecked: boolean = false;
   listaConcepto: ModelCombo[] = [];
-  listaMoneda: ModelCombo[] = [];
-  dataSource = new MatTableDataSource<GastoElement>(dataSourceList);  
-  displayedColumns: string[] = ['accion', 'concepto', 'cantidad', 'nromeses', 'factor', 'moneda', 'montounitmenusal', 'montototalmensual'];
+  //listamoneda_id: ModelCombo[] = [];
+  listaMoneda = [];
+  dataSourceList: any = [];
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['concepto', 'cantidad', 'nromeses', 'factor', 'moneda', 'montounitmenusal', 'montototalmensual', 'accion'];
 
+  @Input() ofertaBase: any = {};
   constructor(
-    //private currencyPipe: CurrencyPipe,
     public dialog: MatDialog,
-    private http: OfertaGastosService
+    private ofertaGastosService: OfertaGastosService,
+    private commonService: CommonService,
   ) { }
 
-  ngOnInit(): void {
-    //Lenar combo Comcepto
-    this.listaConcepto.push(new ModelCombo("1", "Gestión del Proyecto", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("2", "Ingeniero Residente", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("3", "Asistente de Proyecto", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("4", "Solarwind", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("5", "Smart VPN", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("6", "Capacitación", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("7", "Dirección IP", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("8", "Servicios Móviles", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("9", "Costo de Venta de Hardware", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("10", "Tráfico seguro por mega", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("11", "Costo Internacional Recurrente", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("12", "Gestión Soc", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("13", "Costo de Traslado", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("14", "Venta de BGAN Explorer 510", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("15", "Venta de Isatphone", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("16", "Penalidad", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("17", "Costo Internacional Único", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("18", "Venta de BGAN HNS9202", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("19", "Costo de Venta Seguridad", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("20", "Traslado VSAT", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("21", "Traslado CCHH+SPCR+SPAT", 0, 'F'));
-    this.listaConcepto.push(new ModelCombo("22", "Otros", 0, 'O'));
-    this.listaConcepto.push(new ModelCombo("23", "Otros2", 0, 'O'));
-    this.listaConcepto.push(new ModelCombo("24", "Otros3", 0, 'O'));
-    this.listaConcepto.push(new ModelCombo("25", "Renting Equipos", 0.45, 'R'));
-    //Lenar combo Moneda
-    this.listaMoneda.push(new ModelCombo("1", "S/."));
-    this.listaMoneda.push(new ModelCombo("2", "$"));
+  async ngOnInit() {
+    //debugger;
+    //console.log(this.ofertaBase);
+    this.ofertaBase.id = 1;
 
-    this.http.getAllOfertaOpex().subscribe(data => {
-      console.log("ofertaopex:");
+    //Lenar combo moneda_id
+    await this.commonService.getTipoMonedaAll().subscribe(data => {
+      this.listaMoneda = data;
+      console.log(this.listaMoneda);
+    });
+
+    //Lenar combo Comcepto  
+    await this.ofertaGastosService.listarConceptoOpex().subscribe(data => {
+      this.listaConcepto = data;
+    });
+
+    await this.ofertaGastosService.obtenerOfertasOpex(this.ofertaBase.id).subscribe((data:OfertaOpex[]) => {
+      if (data != null)
+        this.dataSource.data = data;
       console.log(data);
     });
   }
@@ -71,29 +58,26 @@ export class OfertaGastosComponent implements OnInit {
       else
         row.mostrarConcepto = false;
 
-        console.log(row.mostrarConcepto);
+      console.log(row.mostrarConcepto);
 
       if (objetoConcepto.factor == 0) {//Si el factor es cero no se mostrara ninguna informacion en la bandeja
         row.factor = 0;
         row.montototalmensual = this.calcularMontoMensual(row);
-        row.montototalmensualParseado = row.montototalmensual.toFixed(2);
       }
       else {
         row.factor = objetoConcepto.factor;
         row.montototalmensual = this.calcularMontoMensual(row);
-        row.montototalmensualParseado = row.montototalmensual.toFixed(2);
       }
     }
   }
   changeMoneda(event, row) {
     if (event.isUserInput) {
       if (event.source.value == "2") {//Cual el tipo de cambio es dolares 
-        row.moneda = "2";
+        row.moneda_id = "2";
       }
-      else// cuando selecciona la moneda de soles
-        row.moneda = "1";
-      row.montototalmensual = this.calcularMontoMensual(row);;//.toFixed(2);
-      row.montototalmensualParseado = row.montototalmensual.toFixed(2);
+      else// cuando selecciona la moneda_id de soles
+        row.moneda_id = "1";
+      row.montototalmensual = this.calcularMontoMensual(row);;//.toFixed(2);      
     }
   }
   inputChangeCantidad(input: string, row: any): void {
@@ -102,58 +86,55 @@ export class OfertaGastosComponent implements OnInit {
     else
       row.cantidad = parseInt(input);
     row.montototalmensual = this.calcularMontoMensual(row);
-    row.montototalmensualParseado = row.montototalmensual.toFixed(2);
-
   }
   inputChangeNumeroMeses(input: string, row: any): void {
     if (input === "")
-      row.nromeses = 0;
+      row.meses = 0;
     else
-      row.nromeses = parseInt(input);
+      row.meses = parseInt(input);
     row.montototalmensual = this.calcularMontoMensual(row);
-    row.montototalmensualParseado = row.montototalmensual.toFixed(2);
   }
 
   inputChangeMontoUnitarioMensual(input: string, row: any): void {
     if (input === "")
-      row.montounitmenusal = 0;
+      row.unitarioMensual = 0;
     else
-      row.montounitmenusal = parseInt(input);
+      row.unitarioMensual = parseInt(input);
     row.montototalmensual = this.calcularMontoMensual(row);
-    row.montototalmensualParseado = row.montototalmensual.toFixed(2); //this.formatMoney(row.montototalmensual)//.toFixed(2);
   }
   calcularMontoMensual(row: any): number {
     var montoCalculado: number = 0;
     if (row.factor > 0)
-      montoCalculado = row.cantidad * row.nromeses * ((row.moneda == "2" ? row.montounitmenusal * this.tipoCambio : row.montounitmenusal) * (row.factor + 1));
+      montoCalculado = row.cantidad * row.meses * ((row.moneda_id == "2" ? row.unitarioMensual * this.tipoCambio : row.unitarioMensual) * (row.factor + 1));
     else
-      montoCalculado = row.cantidad * row.nromeses * (row.moneda == "2" ? row.montounitmenusal * this.tipoCambio : row.montounitmenusal);
+      montoCalculado = row.cantidad * row.meses * (row.moneda_id == "2" ? row.unitarioMensual * this.tipoCambio : row.unitarioMensual);
     return montoCalculado;
   }
   //summaryIncome: item.incomes.reduce((acc, income) => acc + income.value, 0).toFixed(2)
   public calcularTotalSoles() {
-    return this.dataSource.data.reduce((accum, curr) => accum + curr.montototalmensual, 0).toFixed(2);
+    return this.dataSource.data.reduce((accum, curr) => accum + curr.totalMensual, 0).toFixed(2);
   }
   public calcularTotalDolares() {
-    return this.dataSource.data.reduce((accum, curr) => accum + (curr.montototalmensual / this.tipoCambio), 0).toFixed(2);
+    return this.dataSource.data.reduce((accum, curr) => accum + (curr.totalMensual / this.tipoCambio), 0).toFixed(2);
   }
-  crearNuevoGastos(id: number): GastoElement {
+  crearNuevoGastos(ofertaOpexId: number, ofertaId: number): GastoElement {
     return {
-      id: id,
-      concepto: '',
+      ofertaOpexId: ofertaOpexId,
+      ofertaId: ofertaId,
+      conceptoId: -1,
       mostrarConcepto: false,
-      nroconcepto: '',
-      conceptootro: '',
+      nombre: '',
       cantidad: 0,
-      nromeses: 0,
+      meses: 0,
       factor: 0,
-      moneda: '1',
-      montounitmenusal: 0,
-      montototalmensual: 0,      
+      moneda_id_id: 1,
+      unitarioMensual: 0,
+      totalMensual: 0,
     };
   }
   addRow(): void {
-    this.dataSource.data.push(this.crearNuevoGastos(this.dataSource.data.length + 1));
+
+    this.dataSource.data.push(this.crearNuevoGastos(this.dataSource.data.length + 1, this.ofertaBase.id));
     this.dataSource.filter = "";
   }
   deleteRow(item: any): void {
@@ -174,28 +155,28 @@ export class OfertaGastosComponent implements OnInit {
         a.click();
         a.remove();
         this.dataSource.data.splice(this.dataSource.data.indexOf(item.id), 1);
-        this.dataSource = new MatTableDataSource<GastoElement>(dataSourceList);
+        this.dataSource = new MatTableDataSource<GastoElement>(this.dataSourceList);
       }
     });
   }
 }
-const dataSourceList: GastoElement[] = [
-  { id: 1, concepto: '', mostrarConcepto: false, nroconcepto: '', conceptootro: '', cantidad: 0, nromeses: 0, factor: 0, moneda: '1', montounitmenusal: 0, montototalmensual: 0 },
-  { id: 2, concepto: '', mostrarConcepto: false, nroconcepto: '', conceptootro: '', cantidad: 0, nromeses: 0, factor: 0, moneda: '1', montounitmenusal: 0, montototalmensual: 0 },
-  { id: 3, concepto: '', mostrarConcepto: false, nroconcepto: '', conceptootro: '', cantidad: 0, nromeses: 0, factor: 0, moneda: '1', montounitmenusal: 0, montototalmensual: 0 }
-];
+// const dataSourceList: GastoElement[] = [
+//   { ofertaOpexId: 1, concepto: '', mostrarConcepto: false, nroconcepto: '', conceptootro: '', cantidad: 0, meses: 0, factor: 0, moneda_id: '1', unitarioMensual: 0, montototalmensual: 0 },
+//   { ofertaOpexId: 2, concepto: '', mostrarConcepto: false, nroconcepto: '', conceptootro: '', cantidad: 0, meses: 0, factor: 0, moneda_id: '1', unitarioMensual: 0, montototalmensual: 0 },
+//   { ofertaOpexId: 3, concepto: '', mostrarConcepto: false, nroconcepto: '', conceptootro: '', cantidad: 0, meses: 0, factor: 0, moneda_id: '1', unitarioMensual: 0, montototalmensual: 0 }
+// ];
 export interface GastoElement {
-  id: number,
-  concepto: string;
+  ofertaOpexId: number,
+  ofertaId: number,
+  conceptoId: number;
   mostrarConcepto: boolean,
-  nroconcepto: string;
-  conceptootro: string;
+  nombre: string;
   cantidad: number;
-  nromeses: number;
+  meses: number;
   factor: number;
-  moneda: string;
-  montounitmenusal: number;
-  montototalmensual: number;  
+  moneda_id_id: number;
+  unitarioMensual: number;
+  totalMensual: number;
 }
 export class ModelCombo {
   constructor(public id?: string, public nombre?: string, public factor?: number, public tipo?: string) {
--- src/app/main/gestionpropuesta/oferta/componentes/oferta-gastos/oferta-gastos.service.ts
+++ src/app/main/gestionpropuesta/oferta/componentes/oferta-gastos/oferta-gastos.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable, BehaviorSubject, empty } from 'rxjs';
-
+import { OfertaOpex  } from '../../../../../model/Common';
+import { map } from 'rxjs/operators';
 @Injectable()
-export class OfertaGastosService{
+export class OfertaGastosService {
 
     constructor(private http: HttpClient) { }
 
-     getAllOfertaOpex():Observable<any>{
-         //return this.http.get<any>('/ofertaopex/findAll');
-         return this.http.post<any>('/ofertaopex/findAll', {  }); 
-     }
+    getAllOfertaOpex(): Observable<any> {
+        
+        return this.http.post<any>('/ofertaopex/findAll', {});
+    }
+    obtenerOfertasOpex(id: number): Observable<OfertaOpex[]> {        
+        return this.http.get<OfertaOpex[]>('/ofertaopex/obtenerOfertasOpex?ofertaId=' + id).pipe(
+            map(res => {                
+                var result: any = res;
+                if(result.msj=="200")
+                    return result.data;
+                return null;              
+            })
+        );
+    }
+    listarConceptoOpex(): Observable<any> {
+        return this.http.post<any>('/ConceptosOpex/findAll',{});
+    }    
 }
+
